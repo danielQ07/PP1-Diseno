a807e41272f7b69868cea976d98e114728d8d36c
Óscar Trejos Esquivel <56702153+oscarandres2@users.noreply.github.com>
2020-06-07 20:36:00 -0600

    MensajeInverso

--- WatsonCloudAPIsWorkshopV2-master/02-WatsonAssistant/projectSource/WatsonAssistantChat/src/main/AplPrueba.java
+++ WatsonCloudAPIsWorkshopV2-master/02-WatsonAssistant/projectSource/WatsonAssistantChat/src/main/AplPrueba.java
@@ -1,6 +1,20 @@
 package main;
 
+import modelo.PalabraInversa;
+import modelo.Mensaje;
+import modelo.MensajeInverso;
+
 public class AplPrueba {
+	
+  public static void main(String[] args) {
+	  
+    Mensaje mensaje1 = new Mensaje("esto es un secreto no lo puedo decir aserpros") ;
+    PalabraInversa palabraInversa = new PalabraInversa();
+    
+    palabraInversa.cifrar(mensaje1);
+    System.out.println(mensaje1.getMensajeCifrado());
+	  
+  }
 	
 
 }
--- WatsonCloudAPIsWorkshopV2-master/02-WatsonAssistant/projectSource/WatsonAssistantChat/src/modelo/MensajeInverso.java
+++ WatsonCloudAPIsWorkshopV2-master/02-WatsonAssistant/projectSource/WatsonAssistantChat/src/modelo/MensajeInverso.java
@@ -1,5 +1,34 @@
 package modelo;
+import modelo.Transposicion;
+import modelo.Mensaje;
 
-public class MensajeInverso {
+public class MensajeInverso extends Transposicion {
+	
+  public Mensaje cifrar(Mensaje pMensaje) {
+		  
+	return mensajeInverso(pMensaje);
+  }
+	  
+  public Mensaje descifrar(Mensaje pMensaje) {
+		  
+    return mensajeInverso(pMensaje);  
+  }
+  
+  private Mensaje mensajeInverso(Mensaje pMensaje) {
+	
+	String textoMensaje = pMensaje.getMensajeViejo();
+    String mensajeInvertido = "";
+    String [] listaPalabras = textoMensaje.split("\\s+");
+    
+    for(int largoMensaje = listaPalabras.length - 1; largoMensaje >=0;largoMensaje--) {
+    	
+    	mensajeInvertido += invertirPalabra(listaPalabras[largoMensaje]) + " ";  	
+    }
+    
+    pMensaje.setMensajeCifrado(mensajeInvertido);
+    return pMensaje;
+  }
+	
+	
 
 }
